839cd14b3d66949c61552d49351c774d0dded52a
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-07-19 19:08:49 -0400

    Fix inspec test and add disabled example

---
+++ examples/disabled/controls/default.rb
@@ -0,0 +1,10 @@
+#
+# inspec_profile::azurerm-log-analytics-workspace-example-disabled
+# controls::default
+#
+# author::cloudsquad
+# description:: Inspec controls for resource group disabled example
+#
+
+include_controls 'azurerm-resource-group'
+include_controls 'azurerm-log-analytics-workspace'
--- examples/disabled/deploy.tf
+++ examples/disabled/deploy.tf
@@ -1,14 +1,16 @@
 module "resource_group" {
   source = "git::ssh://git@scm.dazzlingwrench.fxinnovation.com:2222/fxinnovation-public/terraform-module-azurerm-resource-group.git?ref=0.1.0"
 
-  name     = "terraform-test-workspace-default"
+  name     = "terraform-test-workspace-disabled"
   location = "canadacentral"
 }
 
 module "default" {
   source = "../../"
 
-  name                = "terraform-test-workspace-default"
+  enabled = false
+
+  name                = "terraform-test-workspace-disabled"
   location            = "canadacentral"
   resource_group_name = module.resource_group.name
   sku                 = "free"
