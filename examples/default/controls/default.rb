b534e3f0221fe76edbd84fdb5c52a16f81453d19
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-07-19 18:44:32 -0400

    Add deffault values

---
+++ examples/default/controls/default.rb
@@ -0,0 +1,10 @@
+#
+# inspec_profile::azurerm-log-analytics-workspace-example-default
+# controls::default
+#
+# author::cloudsquad
+# description:: Inspec controls for resource group default example
+#
+
+include_controls 'azurerm-resource-group'
+include_controls 'azurerm-log-analytics-workspace'
