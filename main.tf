f0aae7e6052404ff6da58b825e61b318b73214f0
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-07-19 16:17:41 -0400

    Fixed variable naming
    I named and declared a variable with different names

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_log_analytics_workspace" "nonfree" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = var.sku
-  retention_in_days   = var.retentionDays
+  retention_in_days   = var.retention_in_days
 
   tags = merge(
     { "Terraform" = "true" },
