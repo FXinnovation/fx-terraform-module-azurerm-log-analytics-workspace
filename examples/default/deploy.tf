e3122cef9dad0d08e1a67a3a407d39fe17697557
Pierre Fortin Carrier <pierre.fortincarrier@gmail.com>
2022-04-13 17:43:58 -0400

    Convert dazzlingwrench Gitea URL to FXI Github Enterprise URL

--- examples/default/deploy.tf
+++ examples/default/deploy.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source = "git::ssh://git@github.com/FXinnovation/fx-terraform-module-azurerm-resource-group.git?ref=0.1.0"
+  source = "github.com/FXinnovation/fx-terraform-module-azurerm-resource-group.git?ref=0.1.0"
 
   name     = "terraform-test-workspace-default"
   location = "canadacentral"
